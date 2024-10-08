f60afd73151bab60cdd05f30823c35f08e679987
Waleed E. <welbashier@hotmail.com>
2024-10-07 23:06:34 -0500

    Initial commit

---
+++ src/main/java/com/gwais/sk_notifications/SkNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.gwais.sk_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkNotificationsApplication.class, args);
+	}
+
+}
